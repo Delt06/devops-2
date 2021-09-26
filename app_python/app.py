476d0e7448ebc103fbc3de74b745fd3b03a6946d
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-09-26 16:32:52 +0300

    Add /visits endpoint

--- app_python/app.py
+++ app_python/app.py
@@ -1,10 +1,29 @@
 from time_utils import get_time_now, format_time
 from flask import Flask
+from os import linesep
+from pathlib import Path
 
 app = Flask(__name__)
+visit_log_path = './visits'
 
 
 @app.route('/')
 def hello():
     time_now = get_time_now()
-    return format_time(time_now)
+    formatted_time = format_time(time_now)
+
+    with open(visit_log_path, "a") as visit_log:
+        visit_log.write(formatted_time)
+        visit_log.write(linesep)
+
+    return formatted_time
+
+
+@app.route('/visits')
+def visits():
+    visit_log = Path(visit_log_path)
+    visit_log.touch(exist_ok=True)
+
+    with open(visit_log_path, "r") as visit_log:
+        file_content = visit_log.read()
+        return "<br>".join(file_content.split('\n'))
